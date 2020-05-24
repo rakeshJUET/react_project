508edcc50eb8733fb8f3b738f4dcb163b90c9534
rakeshJUET <55741290+rakeshJUET@users.noreply.github.com>
2020-05-24 19:17:02 +0530

    Add files via upload

---
+++ week4_react_assignment/src/components/LoadingComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Loading = () => {
+    return (
+        <div className="col-12">
+            <span className="fa fa-spinner fa-pulse  fa- fa-fw tect-primary"></span>
+            <p>Loading....</p>
+        </div>
+    );
+}
---
+++ week4_react_assignment/src/components/MenuComponent.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Card, CardImg, CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap'
+import {Link} from 'react-router-dom'
+import {Loading} from './LoadingComponent'
+import {baseUrl} from '../shared/baseUrl'
+
+
+const Menu = (props)=> {
+        const menu=props.dishes.dishes.map(dish=>{
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card>
+                    <Link to={`/menu/${dish.id}`}>
+                    <CardImg width="100%" src={baseUrl+dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                    </ Link>
+                </Card>
+                </div>
+            );
+        });
+
+        if(props.dishes.isLoading)
+        {
+            return(
+                <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+                </div>
+            );
+        }
+        else if(props.dishes.errMess)
+        {
+            return(
+                <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
+                </div>
+                </div>
+            );
+        }
+        else
+            {
+                return ( 
+                <div className="container">
+                <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                <h3>Menu</h3>
+                </div>
+                </div>
+                <div className="row">
+
+                {menu}
+
+                </div>
+                </div>
+            );
+        }
+    }
+ 
+export default Menu;
